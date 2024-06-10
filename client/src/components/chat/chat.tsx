76c39c879799e8cf2f37f0a4059b8b7ec104af06
Mateus Pierre <mateus.mateus30@gmail.com>
2024-06-10 12:38:37 -0300

    Update chat.tsx

--- client/src/components/chat/chat.tsx
+++ client/src/components/chat/chat.tsx
@@ -1,12 +1,26 @@
-import React,{useRef, useState} from 'react';
+import React,{useRef, useState, useEffect} from 'react';
 
 interface ChatProps {
     socket: any; // Altere 'any' para o tipo correto do seu socket, se disponível
 }
 
+interface Message {
+    author: string;
+    text: string;
+}
+
 const Chat: React.FC<ChatProps> = ({socket}) => {
     const messageRef = useRef();
-    const [messageList, setMessageList] = useState([])
+    const [messageList, setMessageList] = useState([]);
+
+    useEffect(() => {
+        socket.on('receive_message', (data) => {
+            console.log(data)
+            setMessageList((current) => [...current, data])
+        })
+
+        return () => socket.off('receive_message');
+    }, [socket])
 
     const handleSubmit = () => {
         const message = messageRef.current.value;
@@ -22,6 +36,11 @@ const Chat: React.FC<ChatProps> = ({socket}) => {
     return (
         <div>
             <h1>chat</h1>
+            {
+                messageList.map((message, index) => (
+                    <p>{message.author}: {message.text}</p>
+                ))
+            }
             <input type='text' placeholder='Mensagem' />
             <button onClick={() => handleSubmit()}>Send</button>
         </div>
